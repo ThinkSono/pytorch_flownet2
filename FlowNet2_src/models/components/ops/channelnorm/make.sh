953a31752a2bc997c8e5d53b74ac6011bad7fa6c
amakropoulos <benuix@gmail.com>
2019-03-19 23:53:38 +0000

    support for more GPU SMs

--- FlowNet2_src/models/components/ops/channelnorm/make.sh
+++ FlowNet2_src/models/components/ops/channelnorm/make.sh
@@ -7,7 +7,7 @@ echo "Compiling channelnorm kernels by nvcc..."
 rm ChannelNorm_kernel.o
 rm -r ../_ext
 
-nvcc -c -o ChannelNorm_kernel.o ChannelNorm_kernel.cu -x cu -Xcompiler -fPIC -gencode arch=compute_52,code=sm_52  -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -I ${TORCH}/lib/include/TH -I ${TORCH}/lib/include/THC
+nvcc -c -o ChannelNorm_kernel.o ChannelNorm_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_30 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_61,code=compute_61 -I ${TORCH}/lib/include/TH -I ${TORCH}/lib/include/THC
 
 cd ../
 $PYTHON build.py
--- FlowNet2_src/models/components/ops/correlation/make.sh
+++ FlowNet2_src/models/components/ops/correlation/make.sh
@@ -9,7 +9,7 @@ echo "Compiling correlation kernels by nvcc..."
 rm correlation_cuda_kernel.o
 rm -r ../_ext
 
-nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -gencode arch=compute_52,code=sm_52  -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61
+nvcc -c -o correlation_cuda_kernel.o correlation_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_30 -gencode arch=compute_30,code=sm_30 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_61,code=compute_61
 
 cd ../
 $PYTHON build.py
